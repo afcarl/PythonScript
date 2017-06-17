226d936d8279ea97ad7647d67e9ca14d3fe68678
Victoria Tom <victoria.w.tom@gmail.com>
2017-06-16 22:05:23 -0700

    Cleaning up the repo

--- Barnacle_Main_Script.py
+++ Barnacle_Main_Script.py
@@ -1,53 +1,29 @@
-#Shweta Kinger 4/11/17
-# coding: utf-8
-
-# In[1]:
-
-# TO BE USED IN BARNACLE
-
-#MODIFIED TO BE LESS COMPLICATED
-
-#This file performs the parsing of 5 datasets here and runs 9 algorithms on the sample.
-# The program will only use the obese/lean samples (omits overweight) and specifically works on Wu, Turnbaugh, HMP,
-# Amish, and Yasunenko studies
+# For use in Barnacle
 
 # To use this function, call the function that loops through classifiers and creates table in other ntbk
-#modify to have parameter settings at the end
-
 #In order to run this file, you must also download Parsing_Dataset and CLassifiers_Accuracies_Script (from git repo)
-
-# In[3]:
 
 #import statements
 import biom
 import sklearn
 import pandas as pd
-#import nbimporter # not sure if need to download
-#from imp import reload
 import Parsing_Dataset as parse #MAKE SURE YOU DOWNLOAD THIS FROM GIT REPO
 import GridSearch_AccuraciesScript as script #MAKE SURE YOU DOWNLOAD THIS GIT REPO
 from datetime import datetime
 
-#reload(parse)
-#reload(script)
-
-# In[8]:
-
-#MODIFY PATH ACCORDINGLY
-
+# modify this path accordingly
 path = "/projects/bariatric_surgery/"
 
-
-# In[9]:
-
-# dictionary of dictionaries to contain file names
+# dictionary of dictionaries to contain file names (for the original 5 studies)
 files = {
     'turnbaugh': {},
     'wu' : {},
     'amish' : {},
     'yatsunenko' : {},
     'HMP' : {},    
-} 
+}
+
+# used for the new study (the Ercolini study)
 newfiles = {
     'new': {}
 }
@@ -76,21 +52,17 @@ files['HMP']['biom'] = path + "HMP.biom"
 newfiles['new']['meta'] = path + "metadata_newstudy.txt"
 newfiles['new']['biom'] = path + "newstudy.biom"
 
-# In[10]:
 
-#run on each study
-
+#run the script on each study
 for study in files:
+
+    #the Amish study needs additional preprocessing
     isAmish = False
     if(study == 'amish'):
 	isAmish = True
+
     X, y = parse.parse_dataset_X(files[study]['meta'], files[study]['biom'], isAmish)
     dataframe = script.gridSearch(study, X, y, 2)
 	
-#finished all studies
+#finished all studies, write results to file
 dataframe.to_csv('accuracytable' + str(datetime.now()) + '.csv')
-
-# In[8]:
-
-# %timeit list(reversed(range(1,1000)))
-
